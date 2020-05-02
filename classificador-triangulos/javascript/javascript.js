8be339ac8e6962dc3c9d15a6026dd49d4292d7cc
Rebeca <becatriz7@gmail.com>
2020-05-02 11:17:42 -0400

    Alterado JavaSript

--- classificador-triangulos/javascript/javascript.js
+++ classificador-triangulos/javascript/javascript.js
@@ -1,7 +1,8 @@
-function resultadoTringulo(lado1, lado2, lado3) {
-  lado1 = document.getElementById("lado1").value
-  lado2 = document.getElementById("lado2").value
-  lado3 = document.getElementById("lado3").value
+function resultadoTringulo() {
+  let lado1 = document.getElementById("lado1").value
+  let lado2 = document.getElementById("lado2").value
+  let lado3 = document.getElementById("lado3").value
+
 
   if ((lado1 === lado2 && lado3 !== lado1 && lado3 !== lado2) ||
       (lado2 === lado3 && lado1 !== lado2 && lado1 !== lado3) ||

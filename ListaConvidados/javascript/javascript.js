faa6f809c410a7e8aadae7faa36ca84114bc87b8
Rebeca <becatriz7@gmail.com>
2020-05-16 11:08:13 -0400

    Adcionado tabela e lista de objetos

--- ListaConvidados/javascript/javascript.js
+++ ListaConvidados/javascript/javascript.js
@@ -1,19 +1,43 @@
-let adicionarConvidado = [];
+let adicionarConvidado = []
 
 function adicionar() {
+
   let nomes = document.getElementById("convidado").value
-  adicionarConvidado.push(nomes)
+  let idade = document.getElementById("idade").value
+  let email = document.getElementById("email").value
+  let convidado = {
+    nome: nomes,
+    idade: idade,
+    email: email
+  };
+  adicionarConvidado.push(convidado)
   atualizar()
 
 }
 
-function atualizar(){
-   let lista = document.getElementById("lista")
-   lista.innerHTML = ""
-   lista.innerHTML = "<h1>Lista atualizada</h1>"
-   for(var i = 0; i < adicionarConvidado.length; i++){
-     lista.innerHTML += `<div></h1><span>${adicionarConvidado[i]}</span><div>`
-     
-   }
+function atualizar() {
+
+  let lista = document.getElementById("lista")
+  lista.innerHTML = ""
+  lista.innerHTML = `<h1>Lista atualizada</h1>`
+ 
+
+  for (var i = 0; i < adicionarConvidado.length; i++) {
+    lista.innerHTML += `
+  <table border=1 cellspacing=0 cellpadding=2 bordercolor="666633">
+    <th colspan="3">Convidado ${i+1}</th>
+  <tr>
+    <th>Nome</th>
+    <th>Idade</th>
+    <th>Email</th>
+  </tr>
+    <tr>
+      <td>${adicionarConvidado[i].nome}</td>
+      <td>${adicionarConvidado[i].idade}</td>
+      <td>${adicionarConvidado[i].email}</td>
+    </tr>
+    </table></br>`
   
+  }
+
 }
